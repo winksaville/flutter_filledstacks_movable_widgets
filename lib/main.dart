0082b5243a5434e649ef9135cdb67c52e0dce208
Wink Saville <wink@saville.com>
2020-03-19 20:01:52 -0700

    Implement movable widgets
    This is the code from [Dane Mackier snippets](https://www.filledstacks.com/snippet/widget-tricks-create-movable-stack-widgets/)

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:random_color/random_color.dart';
 
-void main() => runApp(HelloWorldApp());
+void main() => runApp(TheApp());
 
-class HelloWorldApp extends StatelessWidget {
+class TheApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Text("Hello, World!"),
+      title: 'Movable Widgets Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeView(),
+    );
+  }
+}
+
+class HomeView extends StatefulWidget {
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  List<Widget> movableItems = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              movableItems.add(MovableStackItem());
+            });
+          },
+        ),
+        body: Stack(
+          children: movableItems,
+        ));
+  }
+}
+
+class MovableStackItem extends StatefulWidget {
+  @override
+  _MovableStackItemState createState() => _MovableStackItemState();
+}
+
+class _MovableStackItemState extends State<MovableStackItem> {
+  double xPosition = 0;
+  double yPosition = 0;
+  Color color;
+
+  @override
+  void initState() {
+    color = RandomColor().randomColor();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: yPosition,
+      left: xPosition,
+      child: GestureDetector(
+        onPanUpdate: (tapInfo) {
+          setState(() {
+            xPosition += tapInfo.delta.dx;
+            yPosition += tapInfo.delta.dy;
+          });
+        },
+        child: Container(
+          width: 150,
+          height: 150,
+          color: color,
         ),
       ),
     );
